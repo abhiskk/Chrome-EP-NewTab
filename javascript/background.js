6354eaf729802b16ba5e771b0241864bc646d2b0
Venkatesh Ganesan <venkatesh920@gmail.com>
2015-09-08 12:01:43 +0530

    modified image url in setBackgroundImage()

--- javascript/background.js
+++ javascript/background.js
@@ -4,7 +4,7 @@ function setBackgroundImage()
 	xmlHttp.onreadystatechange = function() {
 		if(xmlHttp.readyState == 4 && xmlHttp.status == 200) {
 			var response = JSON.parse(xmlHttp.response);
-			var imageUrl = response.data.children[0].data.url;
+			var imageUrl = response.data.children[0].data.preview.images[0].source.url;
 			document.body.style.backgroundImage = "url(" + imageUrl + ")";
 		}
 	}
