8375e7af62100cc376b33bb97c037f8c8d92451b
abhishek kadian <abhishekkadiyan@gmail.com>
2015-09-11 13:18:02 +0530

    Refreshing the images after every 10 new tabs openend.

--- javascript/photo.js
+++ javascript/photo.js
@@ -18,17 +18,22 @@ backgrounds.Photo = new Model({
             if (xmlHttp.readyState == 4 && xmlHttp.status == 200) {
                 if (localStorage.getItem("index") === null) {
                     localStorage.setItem("index", 0);
+                }
+                var index = localStorage.getItem("index");
+                if (Number(index) % 10 == 0) {
+                    console.log("Loading images.");
                     var response = JSON.parse(xmlHttp.response);
                     for (i = 0; i < limitImages; i++) {
                         var imageUrl = response.data.children[i].data.preview.images[0].source.url;
                         localStorage.setItem("url" + i, imageUrl);
                         imageData = new Image();
                         imageData.src = imageUrl;
+
                     }
                 }
-                var index = localStorage.getItem("index");
-                localStorage.setItem("index", (Number(index) + 1) % limitImages);
-                document.body.style.background = "url(" + localStorage.getItem("url" + index) + ") no-repeat center center fixed";
+                console.log(index);
+                localStorage.setItem("index", (Number(index) + 1));
+                document.body.style.background = "url(" + localStorage.getItem("url" + index % limitImages) + ") no-repeat center center fixed";
                 document.body.style.backgroundSize = "cover";
             }
         }
