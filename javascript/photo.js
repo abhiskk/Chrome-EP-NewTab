14619d9e7a4158f93bc03195fe1c0b5ca6461b97
abhishek kadian <abhishekkadiyan@gmail.com>
2015-09-14 23:08:21 +0530

    Positioning share-buttons vertically.

--- javascript/photo.js
+++ javascript/photo.js
@@ -20,21 +20,34 @@ backgrounds.Photo = new Model({
     },
 
     /**
+     * Load first image.
+     */
+    loadFirstImage: function() {
+        var order = this.permutation(5);
+        localStorage.setItem("count", 0);
+        localStorage.setItem("index", 0);
+        localStorage.setItem("image1", order[1]);
+        localStorage.setItem("image2", order[2]);
+        document.body.style.background = "url(" + "/images/backup-wallpapers/image" + order[0] + ".jpg" + ") no-repeat center center fixed";
+        document.body.style.backgroundSize = "cover";
+        document.getElementById("twitter_button").style.visibility = "visible";
+        document.getElementById("facebook_button").style.visibility = "visible"
+    },
+
+    /**
      * Displays the image.
      */
-    display: function(limitImages, cacheSize) {
+        display: function(limitImages, cacheSize) {
+        document.getElementById("twitter_button").style.visibility = "hidden";
+        document.getElementById("facebook_button").style.visibility = "hidden"
         count = localStorage.getItem("count");
         if (count === null) {
-            var order = this.permutation(5);
-            localStorage.setItem("count", 0);
-            localStorage.setItem("index", 0);
-            localStorage.setItem("image1", order[1]);
-            localStorage.setItem("image2", order[2]);
-            document.body.style.background = "url(" + "/images/backup-wallpapers/image" + order[0] + ".jpg" + ") no-repeat center center fixed";
-            document.body.style.backgroundSize = "cover";
+            this.loadFirstImage();
         } else if (Number(count) < 3) {
             document.body.style.background = "url(" + "/images/backup-wallpapers/image" + localStorage.getItem("image" + count) + ".jpg" + ") no-repeat center center fixed";
             document.body.style.backgroundSize = "cover";
+            document.getElementById("twitter_button").style.visibility = "visible";
+            document.getElementById("facebook_button").style.visibility = "visible"
         }
         var index = localStorage.getItem("index");
         var xmlHttp = new XMLHttpRequest();
@@ -42,6 +55,7 @@ backgrounds.Photo = new Model({
             if (xmlHttp.readyState == 4 && xmlHttp.status == 200) {
                 var response = JSON.parse(xmlHttp.response);
                 if (Number(count) < 3) {
+                    // Caching top images repeatedly.
                     for (i = 0; i < cacheSize; i++) {
                         var imageUrl = response.data.children[i].data.preview.images[0].source.url;
                         localStorage.setItem("url" + i, imageUrl);
@@ -49,6 +63,7 @@ backgrounds.Photo = new Model({
                         imageData.src = imageUrl;
                     }
                 } else {
+                    // Caching images to be displayed.
                     for (i = 0; i < cacheSize; i++) {
                         var nextIndex = (Number(index) + 1 + i) % limitImages;
                         var imageUrl = response.data.children[nextIndex].data.preview.images[0].source.url;
@@ -63,18 +78,15 @@ backgrounds.Photo = new Model({
                     document.body.style.background = "url(" + localStorage.getItem("url" + index % limitImages) + ") no-repeat center center fixed";
                     document.body.style.backgroundSize = "cover";
                     if (localStorage.getItem("author" + index % limitImages) != null) {
-
                         // Display text pertaining to the image at the bottom of the page.
                         var author = localStorage.getItem("author" + index % limitImages);
                         var title = localStorage.getItem("title" + index % limitImages);
-
                         // Regex matching
                         var myRe = /\[?o?c?\]?\s*\[?\d+,?\d*\s*x\s*\d+,?\d*\s*\]?\s*\[?o?c?\]?/gi;
                         var myArray = myRe.exec(title);
                         if (myArray != null) {
                             title = title.slice(0, myArray.index) + title.slice(myRe.lastIndex);
                         }
-
                         var div = document.createElement('div');
                         var span1 = document.createElement('span');
                         var text1 = document.createTextNode(title.trim());
@@ -95,6 +107,8 @@ backgrounds.Photo = new Model({
                         div.appendChild(span2);
                         document.body.appendChild(div);
                     }
+                    document.getElementById("twitter_button").style.visibility = "visible";
+                    document.getElementById("facebook_button").style.visibility = "visible"
                     localStorage.setItem("index", (Number(index) + 1));
                 }
             }
